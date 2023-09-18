b6c2713ddbc2d787d00dccfd89d8bb7ab208ecb5
yankoshbadal <yankoshbadal@gmail.com>
2023-09-19 01:21:12 +0530

    files uploaded

---
+++ main.js
@@ -0,0 +1,10 @@
+/*$(document).ready(function() {
+
+	var page_url = window.location.href;
+	var page_id = page_url.substring(page_url.lastIndexOf("#") + 1);
+	if (page_id == "hm") {
+		$("html, body").animate({
+			scrollTop: $("#menu").offset().top - 65
+		}, 4000);
+	}
+});
